d2efab1c488b34703f31305290b066e9fecaa162
Ahmed Rezk <work72019@gmail.com>
2020-06-20 08:34:58 -0600

    Basic Authentication

--- node-express/server.js
+++ node-express/server.js
@@ -13,6 +13,31 @@ app.use(morgan("dev"));
 
 app.use(bodyParser.json());
 
+function auth(req, res, next) {
+	console.log(req.headers);
+	const authHeader = req.headers.authorization;
+	if (!authHeader) {
+		const err = new Error("You are not authenticated!");
+		res.setHeader("WWW-Authenticate", "Basic");
+		err.status = 401;
+		return next(err);
+	}
+
+	const auth = Buffer.from(authHeader.split(" ")[1], "base64").toString().split(":");
+	const user = auth[0];
+	const pass = auth[1];
+	if (user === "admin" && pass === "password") {
+		return next(); // authorized
+	} else {
+		const err = new Error("You are not authenticated!");
+		res.setHeader("WWW-Authenticate", "Basic");
+		err.status = 401;
+		return next(err);
+	}
+}
+
+app.use(auth);
+
 app.use("/campsites", campsiteRouter);
 app.use("/promotions", promotionsRouter);
 app.use("/partners", partnersRouter);
