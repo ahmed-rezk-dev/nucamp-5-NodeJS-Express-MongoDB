ca140ffb831708e3e1f8b01d379abc111dabf8e2
Ahmed Rezk <work72019@gmail.com>
2020-06-20 08:55:08 -0600

    Express Session Part 2

--- node-express/server.js
+++ node-express/server.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const campsiteRouter = require("./routes/campsiteRouter");
 const promotionsRouter = require("./routes/promotionsRouter");
 const partnersRouter = require("./routes/partnersRouter");
+const usersRouter = require("./routes/users");
 const cookieParser = require("cookie-parser");
 const session = require("express-session");
 const FileStore = require("session-file-store")(session);
@@ -25,6 +26,11 @@ app.use(
 		store: new FileStore(),
 	})
 );
+
+app.use("/users", usersRouter);
+app.use("/campsites", campsiteRouter);
+app.use("/promotions", promotionsRouter);
+app.use("/partners", partnersRouter);
 
 function auth(req, res, next) {
 	console.log(req.session);
@@ -67,29 +73,14 @@ app.use(auth);
 app.use(cookieParser("12345-67890-09876-54321"));
 
 function auth(req, res, next) {
-	if (!req.signedCookies.user) {
-		const authHeader = req.headers.authorization;
-		if (!authHeader) {
-			const err = new Error("You are not authenticated!");
-			res.setHeader("WWW-Authenticate", "Basic");
-			err.status = 401;
-			return next(err);
-		}
+	console.log(req.session);
 
-		const auth = Buffer.from(authHeader.split(" ")[1], "base64").toString().split(":");
-		const user = auth[0];
-		const pass = auth[1];
-		if (user === "admin" && pass === "password") {
-			res.cookie("user", "admin", { signed: true });
-			return next(); // authorized
-		} else {
-			const err = new Error("You are not authenticated!");
-			res.setHeader("WWW-Authenticate", "Basic");
-			err.status = 401;
-			return next(err);
-		}
+	if (!req.session.user) {
+		const err = new Error("You are not authenticated!");
+		err.status = 401;
+		return next(err);
 	} else {
-		if (req.signedCookies.user === "admin") {
+		if (req.session.user === "authenticated") {
 			return next();
 		} else {
 			const err = new Error("You are not authenticated!");
@@ -98,10 +89,6 @@ function auth(req, res, next) {
 		}
 	}
 }
-
-app.use("/campsites", campsiteRouter);
-app.use("/promotions", promotionsRouter);
-app.use("/partners", partnersRouter);
 
 app.use(express.static(__dirname + "/public"));
 
