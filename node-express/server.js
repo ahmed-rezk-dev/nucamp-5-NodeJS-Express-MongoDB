085e049d8c7b31c6e4eabe17c5e67a2d148ed113
Ahmed Rezk <work72019@gmail.com>
2020-06-20 08:48:16 -0600

    Express Session Part 1

--- node-express/server.js
+++ node-express/server.js
@@ -4,6 +4,9 @@ const bodyParser = require("body-parser");
 const campsiteRouter = require("./routes/campsiteRouter");
 const promotionsRouter = require("./routes/promotionsRouter");
 const partnersRouter = require("./routes/partnersRouter");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const FileStore = require("session-file-store")(session);
 
 const hostname = "localhost";
 const port = 5000;
@@ -13,30 +16,88 @@ app.use(morgan("dev"));
 
 app.use(bodyParser.json());
 
-function auth(req, res, next) {
-	console.log(req.headers);
-	const authHeader = req.headers.authorization;
-	if (!authHeader) {
-		const err = new Error("You are not authenticated!");
-		res.setHeader("WWW-Authenticate", "Basic");
-		err.status = 401;
-		return next(err);
-	}
+app.use(
+	session({
+		name: "session-id",
+		secret: "12345-67890-09876-54321",
+		saveUninitialized: false,
+		resave: false,
+		store: new FileStore(),
+	})
+);
 
-	const auth = Buffer.from(authHeader.split(" ")[1], "base64").toString().split(":");
-	const user = auth[0];
-	const pass = auth[1];
-	if (user === "admin" && pass === "password") {
-		return next(); // authorized
+function auth(req, res, next) {
+	console.log(req.session);
+
+	if (!req.session.user) {
+		const authHeader = req.headers.authorization;
+		if (!authHeader) {
+			const err = new Error("You are not authenticated!");
+			res.setHeader("WWW-Authenticate", "Basic");
+			err.status = 401;
+			return next(err);
+		}
+
+		const auth = Buffer.from(authHeader.split(" ")[1], "base64").toString().split(":");
+		const user = auth[0];
+		const pass = auth[1];
+		if (user === "admin" && pass === "password") {
+			req.session.user = "admin";
+			return next(); // authorized
+		} else {
+			const err = new Error("You are not authenticated!");
+			res.setHeader("WWW-Authenticate", "Basic");
+			err.status = 401;
+			return next(err);
+		}
 	} else {
-		const err = new Error("You are not authenticated!");
-		res.setHeader("WWW-Authenticate", "Basic");
-		err.status = 401;
-		return next(err);
+		if (req.session.user === "admin") {
+			console.log("req.session:", req.session);
+			return next();
+		} else {
+			const err = new Error("You are not authenticated!");
+			err.status = 401;
+			return next(err);
+		}
 	}
 }
 
 app.use(auth);
+
+app.use(cookieParser("12345-67890-09876-54321"));
+
+function auth(req, res, next) {
+	if (!req.signedCookies.user) {
+		const authHeader = req.headers.authorization;
+		if (!authHeader) {
+			const err = new Error("You are not authenticated!");
+			res.setHeader("WWW-Authenticate", "Basic");
+			err.status = 401;
+			return next(err);
+		}
+
+		const auth = Buffer.from(authHeader.split(" ")[1], "base64").toString().split(":");
+		const user = auth[0];
+		const pass = auth[1];
+		if (user === "admin" && pass === "password") {
+			res.cookie("user", "admin", { signed: true });
+			return next(); // authorized
+		} else {
+			const err = new Error("You are not authenticated!");
+			res.setHeader("WWW-Authenticate", "Basic");
+			err.status = 401;
+			return next(err);
+		}
+	} else {
+		if (req.signedCookies.user === "admin") {
+			return next();
+		} else {
+			const err = new Error("You are not authenticated!");
+			err.status = 401;
+			return next(err);
+		}
+	}
+}
 
 app.use("/campsites", campsiteRouter);
 app.use("/promotions", promotionsRouter);
